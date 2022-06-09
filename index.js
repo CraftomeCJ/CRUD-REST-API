d1448718d8f8d55ca5a0df85c0721d4f311d1608
javier ho <tech@craftome.com>
2022-06-09 12:22:28 +0800

    a functioning API server that runs on Node.js and is hooked up to an active PostgreSQL database

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+// to get all the exported functions from queries.js, I'll require the file and assign it to a variable here
+const db = require('./queries')
+
+// I'll require the express module, the built-in body-parser middleware, and I'll set our app and port variables
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 4001
 
+// body-parser middleware syntax
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -10,10 +15,19 @@ app.use(
   })
 )
 
+// tell a route to look for a GET request on the root / URL and return some JSON
 app.get('/', (req, res) => {
   res.json({info: 'Node.js, Express, and Postgres API'})
 })
 
+// for each endpoint, let's set the HTTP request method, the endpoint URL path, and the relevant function:
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.deleteUser)
+app.delete('/users/:id', db.deleteUser)
+
+// set the app to listen on the port I set:
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
--- queries.js
+++ queries.js
@@ -1,21 +1,26 @@
+
+// connecting to a Postgres database from Node.js
 const Pool = require('pg').Pool
+
 const pool = new Pool({
   user: 'admin',
   host: 'localhost',
   database: 'api',
   password: '12345',
-  port: 4001,
+  port: 5432,
 })
 
+// creating routes for CRUD operations:
 // GET all users
 const getUsers = (req, res) => {
   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-    if (err) {
+    if (error) {
       throw error
     }
-    res.stauts(200).json(results.rows)
+    res.status(200).json(results.rows)
   })
 }
+
 
 // GET a single user by ID
 const getUserById = (req, res) => {
@@ -28,4 +33,53 @@ const getUserById = (req, res) => {
     res.status(200).json(results.rows)
   })
 
+}
+
+// POST a new user
+const createUser = (req, res) => {
+  const { name, email } = req.body
+
+  pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(201).send(`User added with ID: ${results.rows[0].id}`)
+  })
+}
+
+// PUT updated data in an existing user
+const updateUser = (req, res) => {
+  const id = parseInt(res.params.id)
+  const { name, email } = req.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      res.status(200).send(`User modified with ID: ${id}`)
+    })
+}
+
+// DELETE a user
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+// exporting CRUD functions in a REST API
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
 }
